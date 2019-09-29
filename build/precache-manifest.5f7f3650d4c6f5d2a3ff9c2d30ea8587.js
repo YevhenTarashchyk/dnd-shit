f9372cc86bc543fded83094e88216a2d42e3f993
Yevhen_Tarashchyk <maze.ultramarine@gmail.com>
2019-09-29 18:47:47 +0300

    build
    build

---
+++ build/precache-manifest.5f7f3650d4c6f5d2a3ff9c2d30ea8587.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34329112f6219a4b923e23c02e6cd489",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8811c0c28ac96800f0e8",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f47c64f8add280732e32",
+    "url": "/static/css/main.725d286d.chunk.css"
+  },
+  {
+    "revision": "8811c0c28ac96800f0e8",
+    "url": "/static/js/2.a9f06e9c.chunk.js"
+  },
+  {
+    "revision": "f47c64f8add280732e32",
+    "url": "/static/js/main.3fd9163c.chunk.js"
+  },
+  {
+    "revision": "69e892700d1ab4a00944",
+    "url": "/static/js/runtime~main.0b893c9b.js"
+  }
+]);
